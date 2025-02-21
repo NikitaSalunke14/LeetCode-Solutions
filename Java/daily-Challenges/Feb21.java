80598525342b04a4ec46008e9ddc57ff65247062
Nikita Sanjay Salunke <106645960+NikitaSalunke14@users.noreply.github.com>
2025-02-21 17:42:47 +0530

    Update Feb21.java

--- Java/daily-Challenges/Feb21.java
+++ Java/daily-Challenges/Feb21.java
@@ -1,21 +1,14 @@
 /* Problem Statement 
  * Given a binary tree with the following rules:
-
 root.val == 0
 For any treeNode:
 If treeNode.val has a value x and treeNode.left != null, then treeNode.left.val == 2 * x + 1
 If treeNode.val has a value x and treeNode.right != null, then treeNode.right.val == 2 * x + 2
 Now the binary tree is contaminated, which means all treeNode.val have been changed to -1.
-
 Implement the FindElements class:
-
 FindElements(TreeNode* root) Initializes the object with a contaminated binary tree and recovers it.
 bool find(int target) Returns true if the target value exists in the recovered binary tree.
- 
-
 Example 1:
-
-
 Input
 ["FindElements","find","find"]
 [[[-1,null,-1]],[1],[2]]
@@ -26,8 +19,6 @@ FindElements findElements = new FindElements([-1,null,-1]);
 findElements.find(1); // return False 
 findElements.find(2); // return True 
 Example 2:
-
-
 Input
 ["FindElements","find","find","find"]
 [[[-1,-1,-1,-1,-1]],[1],[3],[5]]
@@ -39,8 +30,6 @@ findElements.find(1); // return True
 findElements.find(3); // return True
 findElements.find(5); // return False
 Example 3:
-
-
 Input
 ["FindElements","find","find","find","find"]
 [[[-1,null,-1,-1,null,-1]],[2],[3],[4],[5]]
@@ -52,10 +41,7 @@ findElements.find(2); // return True
 findElements.find(3); // return False
 findElements.find(4); // return False
 findElements.find(5); // return True
- 
-
 Constraints:
-
 TreeNode.val == -1
 The height of the binary tree is less than or equal to 20
 The total number of nodes is between [1, 104]
@@ -79,7 +65,6 @@ Total calls of find() is between [1, 104]
  */
 class FindElements {
     BitSet recoveredValues;
-
     public FindElements(TreeNode root) {
         root.val =0;
         recoveredValues =new BitSet();
@@ -98,16 +83,13 @@ class FindElements {
             root.right.val = 2 *root.val +2;
             recoverTree(root.right);
         }
-    }
-    
+    }   
     public boolean find(int target) {
         return recoveredValues.get(target);
     }
 }
-
 /**
  * Your FindElements object will be instantiated and called as such:
  * FindElements obj = new FindElements(root);
  * boolean param_1 = obj.find(target);
  */
-
